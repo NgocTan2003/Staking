58d3cdf9cfa49f191dd6b19c77885f86687ec553
tân nguyễn <tan0992727@gmail.com>
2025-05-26 22:22:31 +0700

    fix dbName

--- BE/src/config/db.ts
+++ BE/src/config/db.ts
@@ -4,7 +4,9 @@ import { MONGO_URI } from "../constants/env";
 
 const connectToDatabase = async () => {
     try {
-        await mongoose.connect(MONGO_URI);
+        const dbName = "staking";  
+        const mongoURIWithDB = `${MONGO_URI}${dbName}`;  
+        await mongoose.connect(mongoURIWithDB);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB", error);
